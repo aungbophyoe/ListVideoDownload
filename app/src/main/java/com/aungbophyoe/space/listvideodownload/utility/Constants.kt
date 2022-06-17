c33b2668bbc0d178f600762a0de007d565b77f08
aungbophyoe <abbp1001@gmail.com>
2022-06-17 14:28:31 +0630

    first commit

---
+++ app/src/main/java/com/aungbophyoe/space/listvideodownload/utility/Constants.kt
@@ -0,0 +1,5 @@
+package com.aungbophyoe.space.listvideodownload.utility
+
+object Constants {
+    const val REQUEST_STORAGE_READ_WRITE_PERMISSION = 1023
+}
---
+++ app/src/main/java/com/aungbophyoe/space/listvideodownload/utility/Utility.kt
@@ -0,0 +1,20 @@
+package com.aungbophyoe.space.listvideodownload.utility
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import pub.devrel.easypermissions.EasyPermissions
+
+object Utility {
+    fun hasStoragePermission(context: Context) =
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            Environment.isExternalStorageManager()
+        }else{
+            EasyPermissions.hasPermissions(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+    fun getCurrentTimeInMillis() = "${System.currentTimeMillis()}"
+}
